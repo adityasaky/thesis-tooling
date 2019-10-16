ebb317b3950aefd7f780ae1d9f8044635a199408
Aditya Saky <aditya@saky.in>
2019-10-16 15:14:35 -0400

    Initial commit
    Signed-off-by: Aditya Saky <aditya@saky.in>

---
+++ make-graph.py
@@ -0,0 +1,37 @@
+import os
+import networkx as nx
+import sys
+import json
+
+
+DATA_ROOT = "data"
+GRAPH_ROOT = "graphs"
+
+
+def make_graph(data):
+    graph = nx.DiGraph()
+
+    for required_package in data:
+        graph.add_node(required_package)
+        for consuming_package in data.get(required_package):
+            graph.add_node(consuming_package)
+            graph.add_edge(consuming_package, required_package)
+
+    return graph
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        data_file_name = "data_2019-10-16.json"
+    else:
+        data_file_name = sys.argv[1]
+
+    data_path = os.path.join(DATA_ROOT, data_file_name)
+
+    with open(data_path, "r") as data_fp:
+        data = json.load(data_fp)
+
+    graph = make_graph(data)
+
+    nx.write_gexf(graph, os.path.join(GRAPH_ROOT, data_file_name + ".gexf"))
+
