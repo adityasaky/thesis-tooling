78fdb85378ef271520e9f36e13e77eb9065bcab5
Aditya Saky <aditya@saky.in>
2019-11-11 15:31:13 -0500

    Add range for ranks
    Signed-off-by: Aditya Saky <aditya@saky.in>

--- find-top-n.py
+++ find-top-n.py
@@ -4,12 +4,16 @@ import sys
 
 
 if __name__ == "__main__":
-    count = int(sys.argv[1])
+    start = 0
+    end = sys.argv[1]
     with open(sys.argv[2], "r") as fp:
         pagerank = json.load(fp)
 
     sorted_pagerank = sorted(pagerank.items(), key=operator.itemgetter(1), reverse=True)
 
-    for i in range(count):
-        print(sorted_pagerank[i])
+    if "-" in end:
+        start, end = end.split("-")
+
+    for i in range(int(start), int(end)):
+        print(i + 1, sorted_pagerank[i][0], sorted_pagerank[i][1])
 
