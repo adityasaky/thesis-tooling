26277dea8808e5cc038a4321d4fabeebd0e2e649
Aditya Saky <aditya@saky.in>
2019-12-10 19:26:28 -0500

    Print rank
    Signed-off-by: Aditya Saky <aditya@saky.in>

--- find-x.py
+++ find-x.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import operator
 
 
 if __name__ == "__main__":
@@ -7,5 +8,10 @@ if __name__ == "__main__":
     with open(sys.argv[2], "r") as fp:
         pagerank = json.load(fp)
 
-    print(pagerank[package])
+    sorted_pagerank = sorted(pagerank.items(), key=operator.itemgetter(1), reverse=True)
+
+    for i in range(len(sorted_pagerank)):
+        if sorted_pagerank[i][0] == package:
+            print(i + 1, sorted_pagerank[i][0], sorted_pagerank[i][1])
+            break
 
